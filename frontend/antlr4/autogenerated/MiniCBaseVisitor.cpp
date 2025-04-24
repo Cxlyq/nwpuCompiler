83c23d1847b3c483cf8bce423b354e35b8c20831
Jev <158244612+JEVector055@users.noreply.github.com>
2025-04-24 07:18:57 +0000

    fix: remove antlr4/autogenerated from git; fix: remove labels "xxReturn" from minic.g4;

--- frontend/antlr4/autogenerated/MiniCBaseVisitor.cpp
+++
@@ -1,7 +0,0 @@
-
-// Generated from /home/cx/文档/codes/nwpuCompiler/frontend/antlr4/MiniC.g4 by ANTLR 4.12.0
-
-
-#include "MiniCBaseVisitor.h"
-
-
--- frontend/antlr4/autogenerated/MiniCVisitor.cpp
+++
@@ -1,5 +0,0 @@
-
-// Generated from /home/cx/文档/codes/nwpuCompiler/frontend/antlr4/MiniC.g4 by ANTLR 4.12.0
-// Generated from /home/code/exp04-minic-expr/frontend/antlr4/MiniC.g4 by ANTLR 4.12.0
-
-#include "MiniCVisitor.h"
