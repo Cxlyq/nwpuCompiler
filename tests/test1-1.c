6d770b0b9d664bdfb044fcc9150ce59cd5693b42
Kevin-wjq13777 <w137776w@gmail.com>
2025-04-26 16:34:57 +0800

    Merge branch 'dev' of https://github.com/Cxlyq/nwpuCompiler into dev

--- tests/test1-1.c
+++
@@ -1,9 +0,0 @@
-
-int add(int k,int w)
-{
-	k=5;
-    int a = 1, b = 2,c=3,e=0;
-    const int d = 4;
-    a = +a*b/c%e;
-    return a + d+k;
-}
--- tests/test2-1.c
+++
@@ -1,6 +0,0 @@
-const int a = 5;
-float temp()
-{
-    int a = 3;
-    int b = 5;
-}
