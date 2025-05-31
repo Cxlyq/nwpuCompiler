397b64aa3718226e72441dc8fdf1b830bbe5eacb
Jev <158244612+JEVector055@users.noreply.github.com>
2025-05-31 03:59:23 +0000

    merge branch 'dev' and recover .gitignore.

--- tools/testFrontend.zsh
+++ tools/testFrontend.zsh
@@ -3,5 +3,5 @@
 # ./build/minic -S -T -o ./tests/test1-1.png ./tests/test1-1.c
 # IR生成
 ./build/minic -S -I -o ./tests/test1-1-0.ir ./tests/test1-1.c
-clang ./tests/test1-1.c -emit-llvm -S -c -o ./tests/test1-1-1.ll
-./tools/IRCompiler_df -S -L -o ./tests/test1-1-2.ll ./tests/test1-1.c
+clang ./tests/test1-1.c -emit-llvm -S -c -o ./tests/test1-1-clang.ll
+./tools/IRCompiler_df -S -L -o ./tests/test1-1-zlj.ll ./tests/test1-1.c
