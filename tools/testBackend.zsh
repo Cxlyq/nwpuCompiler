930728ff8ee69da0b52e4b0b39a41b34379edd7f
拼好码 <t202510699205750@eduxiji.net>
2025-07-05 10:42:17 +0000

    basic release structure

--- tools/testBackend.zsh
+++
@@ -1,4 +0,0 @@
-
-./build/minic -S -o tests/test1-1-1.s tests/test1-1.c
-#使用clang生成汇编
-clang -target riscv64 -march=rv64imafdc -mabi=lp64d -S tests/test1-1.c -o tests/test1-1-2.s -O0
--- tools/testFrontend.zsh
+++
@@ -1,7 +0,0 @@
-# 建议复制粘贴运行，不要直接运行整个文件
-# 抽象语法树
-# ./build/minic -S -T -o ./tests/test1-1.png ./tests/test1-1.c
-# IR生成
-./build/minic -S -I -o ./tests/test1-1-0.ll ./tests/test1-1.c
-clang ./tests/test1-1.c -emit-llvm -S -c -o ./tests/test1-1-clang.ll
-#./tools/IRCompiler_df -S -L -o ./tests/test1-1-zlj.ll ./tests/test1-1.c
