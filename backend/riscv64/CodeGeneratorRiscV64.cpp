5f150aaae53e73168ca629d4da5c9143bc95beb4
拼好码 <t202510699205750@eduxiji.net>
2025-07-05 05:16:43 +0000

    fix:change code into 16 byte.

--- backend/riscv64/CodeGeneratorRiscV64.cpp
+++ backend/riscv64/CodeGeneratorRiscV64.cpp
@@ -47,7 +47,7 @@ void CodeGeneratorRiscV64::genHeader()
 {
     // 设置 RISC-V 架构：64-bit, 包含整数、乘法、原子、浮点、压缩指令扩展
     fprintf(fp, ".text\n");
-    fprintf(fp, ".attribute 4, 15\n"); // lp64d ABI，或者根据你的工具链确认
+    fprintf(fp, ".attribute 4, 16\n"); // lp64d ABI，或者根据你的工具链确认
     fprintf(fp, ".attribute 5, \"rv64i2p0_m2p0_a2p0_f2p0_d2p0_c2p0\"\n"); // 包含 d 扩展
     // TODO 是否需要输出源文件
     //  fprintf(fp, ".file \"generated_code.c\"\n");
